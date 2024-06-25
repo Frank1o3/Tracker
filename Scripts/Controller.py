84fb1bd3836a40dbb8a5dd36326aac6364eccda3
Frank1o3 <140134525+Frank1o3@users.noreply.github.com>
2024-06-25 13:33:23 -0400

    new file:   .gitignore 	new file:   Scripts/Controller.py 	new file:   Scripts/Main.py 	new file:   Scripts/__pycache__/Controller.cpython-312.pyc 	new file:   Scripts/__pycache__/Mouse.cpython-312.pyc 	new file:   Scripts/__pycache__/Movement.cpython-312.pyc 	new file:   Scripts/test.py 	new file:   images/point.png 	new file:   info.txt 	new file:   requirements.txt

---
+++ Scripts/Controller.py
@@ -0,0 +1,245 @@
+import ctypes
+import time
+
+
+class VirtualMouse:
+    """
+    A class to simulate mouse movements and clicks using ctypes and Windows API.
+    """
+
+    class POINT(ctypes.Structure):
+        """
+        Structure to represent a point with x and y coordinates.
+        """
+
+        _fields_ = [("x", ctypes.c_long), ("y", ctypes.c_long)]
+
+    def __init__(self) -> None:
+        """
+        Initialize the VirtualMouse object with necessary constants for mouse events.
+        """
+        self.MOUSEEVENTF_MOVE = 0x0001
+        self.MOUSEEVENTF_LEFTDOWN = 0x0002
+        self.MOUSEEVENTF_LEFTUP = 0x0004
+        self.MOUSEEVENTF_RIGHTDOWN = 0x0008
+        self.MOUSEEVENTF_RIGHTUP = 0x0010
+
+    def get_cursor_position(self):
+        """
+        Get the current cursor position on the screen.
+
+        Returns:
+            tuple: (x, y) coordinates of the cursor.
+        """
+        pt = VirtualMouse.POINT()
+        ctypes.windll.user32.GetCursorPos(ctypes.byref(pt))
+        return pt.x, pt.y
+
+    def move_mouse_relative(self, dx: int = 0, dy: int = 0):
+        """
+        Move the mouse cursor relative to its current position.
+
+        Args:
+            dx (int): Horizontal movement in pixels.
+            dy (int): Vertical movement in pixels.
+        """
+        x_orig, y_orig = self.get_cursor_position()
+        ctypes.windll.user32.mouse_event(self.MOUSEEVENTF_MOVE, dx, dy, 0, 0)
+        ctypes.windll.user32.SetCursorPos(x_orig + dx, y_orig + dy)
+
+    def left_click(self):
+        """
+        Perform a left mouse button click.
+        """
+        ctypes.windll.user32.mouse_event(self.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        time.sleep(0.1)
+        ctypes.windll.user32.mouse_event(self.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+
+    def right_click(self):
+        """
+        Perform a right mouse button click.
+        """
+        ctypes.windll.user32.mouse_event(self.MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0)
+        time.sleep(0.1)
+        ctypes.windll.user32.mouse_event(self.MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0)
+
+
+class VirtualKeyboard:
+    """
+    A class to simulate keyboard key presses using ctypes and Windows API.
+
+    Attributes:
+        KEYEVENTF_EXTENDEDKEY (int): Constant for key down event.
+        KEYEVENTF_KEYUP (int): Constant for key up event.
+        char_to_keycode_map (dict): Mapping of characters to their virtual key codes.
+    """
+
+    KEYEVENTF_EXTENDEDKEY = 0x0001
+    KEYEVENTF_KEYUP = 0x0002
+
+    char_to_keycode_map = {
+        "a": 0x41,
+        "b": 0x42,
+        "c": 0x43,
+        "d": 0x44,
+        "e": 0x45,
+        "f": 0x46,
+        "g": 0x47,
+        "h": 0x48,
+        "i": 0x49,
+        "j": 0x4A,
+        "k": 0x4B,
+        "l": 0x4C,
+        "m": 0x4D,
+        "n": 0x4E,
+        "o": 0x4F,
+        "p": 0x50,
+        "q": 0x51,
+        "r": 0x52,
+        "s": 0x53,
+        "t": 0x54,
+        "u": 0x55,
+        "v": 0x56,
+        "w": 0x57,
+        "x": 0x58,
+        "y": 0x59,
+        "z": 0x5A,
+        "0": 0x30,
+        "1": 0x31,
+        "2": 0x32,
+        "3": 0x33,
+        "4": 0x34,
+        "5": 0x35,
+        "6": 0x36,
+        "7": 0x37,
+        "8": 0x38,
+        "9": 0x39,
+        " ": 0x20,  # Space
+        "enter": 0x0D,  # Enter
+        "left_shift": 0xA0,  # Left Shift
+        "right_shift": 0xA1,  # Right Shift
+        "left_ctrl": 0xA2,  # Left Control
+        "right_ctrl": 0xA3,  # Right Control
+        "alt": 0x12,  # Alt
+        "esc": 0x1B,  # Escape
+        "backspace": 0x08,  # Backspace
+        "tab": 0x09,  # Tab
+        "capslock": 0x14,  # Caps Lock
+        "left_arrow": 0x25,  # Left Arrow
+        "up_arrow": 0x26,  # Up Arrow
+        "right_arrow": 0x27,  # Right Arrow
+        "down_arrow": 0x28,  # Down Arrow
+    }
+
+    def __init__(self) -> None:
+        """
+        Initializes a VirtualKeyboard instance.
+        """
+        self.user32 = ctypes.WinDLL("user32", use_last_error=True)
+
+    def char_to_keycode(self, char: str) -> int:
+        """
+        Converts a character to its corresponding virtual key code.
+
+        Args:
+            char (str): The character to convert.
+
+        Returns:
+            int: The virtual key code.
+        """
+        return self.char_to_keycode_map.get(char.lower(), None)
+
+    def key_down(self, hexKeyCode: int):
+        """
+        Simulates pressing a key down.
+
+        Args:
+            hexKeyCode (int): The virtual key code of the key to press.
+        """
+        self.user32.keybd_event(hexKeyCode, 0, self.KEYEVENTF_EXTENDEDKEY, 0)
+
+    def key_up(self, hexKeyCode: int):
+        """
+        Simulates releasing a key.
+
+        Args:
+            hexKeyCode (int): The virtual key code of the key to release.
+        """
+        self.user32.keybd_event(
+            hexKeyCode, 0, self.KEYEVENTF_EXTENDEDKEY | self.KEYEVENTF_KEYUP, 0
+        )
+
+    def press_key(self, hexKeyCode: int):
+        """
+        Simulates pressing and releasing a key.
+
+        Args:
+            hexKeyCode (int): The virtual key code of the key to press and release.
+        """
+        self.key_down(hexKeyCode)
+        self.key_up(hexKeyCode)
+
+    def toggle_capslock(self):
+        """
+        Toggles the Caps Lock key state.
+        """
+        self.user32.keybd_event(0x14, 0, self.KEYEVENTF_EXTENDEDKEY, 0)
+        self.user32.keybd_event(
+            0x14, 0, self.KEYEVENTF_EXTENDEDKEY | self.KEYEVENTF_KEYUP, 0
+        )
+
+    def type_string(self, input_string: str):
+        """
+        Types a string by pressing each character sequentially.
+
+        Args:
+            input_string (str): The string to type.
+        """
+        for char in input_string:
+            keycode = self.char_to_keycode(char)
+            if keycode is not None:
+                self.press_key(keycode)
+            else:
+                raise ValueError(
+                    f"Character '{char}' does not have a virtual key code mapping."
+                )
+
+    def type_key_combination(self, *args):
+        """
+        Simulates typing a key combination (e.g., Ctrl+C, Ctrl+V).
+
+        Args:
+            *args: Variable length list of key names to press in combination.
+                   Supported key names: 'ctrl', 'shift', 'alt', and any character key.
+        """
+        for key in args:
+            if key.lower() == "ctrl":
+                self.key_down(self.char_to_keycode("left_ctrl"))
+            elif key.lower() == "shift":
+                self.key_down(self.char_to_keycode("left_shift"))
+            elif key.lower() == "alt":
+                self.key_down(self.char_to_keycode("alt"))
+            else:
+                keycode = self.char_to_keycode(key)
+                if keycode is not None:
+                    self.press_key(keycode)
+                else:
+                    raise ValueError(
+                        f"Key '{key}' does not have a virtual key code mapping."
+                    )
+        time.sleep(0.1)  # Optional delay between key presses
+        for key in args[::-1]:
+            if key.lower() == "ctrl":
+                self.key_up(self.char_to_keycode("left_ctrl"))
+            elif key.lower() == "shift":
+                self.key_up(self.char_to_keycode("left_shift"))
+            elif key.lower() == "alt":
+                self.key_up(self.char_to_keycode("alt"))
+            else:
+                keycode = self.char_to_keycode(key)
+                if keycode is not None:
+                    self.key_up(keycode)
+                else:
+                    raise ValueError(
+                        f"Key '{key}' does not have a virtual key code mapping."
+                    )
---
+++ Scripts/test.py
@@ -0,0 +1,129 @@
+from Controller import VirtualMouse, VirtualKeyboard
+from threading import Thread, Event, Lock
+from PIL import ImageGrab
+import keyboard as kb
+from mss import mss
+import numpy as np
+import cv2 as cv
+import math
+
+TOX = 0
+TOY = 0
+FOV = 350
+STOP = False
+FRAME = None
+POSITIONS = []
+THRESHOLD = 0.74
+SENSITIVITY = 5
+FRAME_LOCK = Lock()
+VM = VirtualMouse()
+VK = VirtualKeyboard()
+
+
+TEMPLATE_IMAGE = cv.imread(
+    "C:/Users/jahdy/OneDrive/Desktop/Tracker/images/point.png", cv.IMREAD_GRAYSCALE
+)
+
+
+def calculate(x: int, to: int, sensitivity: int) -> int:
+    return math.ceil((to - x) // sensitivity)
+
+
+def screenshot(event: Event):
+    sct = mss()
+    monitor = sct.monitors[0]
+    global FRAME
+    while not event.is_set():
+        x = (monitor["width"] - FOV) // 2
+        y = (monitor["height"] - FOV) // 2
+        monitor_area = (x, y, x + FOV, y + FOV)
+        img = ImageGrab.grab(monitor_area)
+        i = np.array(img)
+        with FRAME_LOCK:
+            FRAME = cv.cvtColor(i, cv.COLOR_RGB2BGR)
+
+
+def detect(event: Event):
+    if TEMPLATE_IMAGE is None:
+        print("Failed to load template image")
+        return
+    TEMPLATE_IMAGE[TEMPLATE_IMAGE == 255] = 0
+    global POSITIONS
+    while not event.is_set():
+        with FRAME_LOCK:
+            if FRAME is None:
+                continue
+            gray_frame = cv.cvtColor(FRAME, cv.COLOR_BGR2GRAY)
+        res = cv.matchTemplate(gray_frame, TEMPLATE_IMAGE, cv.TM_CCOEFF_NORMED)
+        loc = np.where(res >= THRESHOLD)
+        POSITIONS = list(zip(*loc[::-1]))
+
+
+def move_aim(event: Event):
+    if TEMPLATE_IMAGE is None:
+        print("Failed to load template image")
+        return
+    w, h = TEMPLATE_IMAGE.shape[::-1]
+    sct = mss()
+    monitor = sct.monitors[0]
+    left = (monitor["width"] - FOV) // 2
+    top = (monitor["height"] - FOV) // 2
+    global TOX, TOY
+    while not event.is_set():
+        if not POSITIONS:
+            continue
+        x, y = POSITIONS[0]
+        x += left + (w // 2) + 4
+        y += top + (h // 2) + 4
+        cursor_x, cursor_y = VM.get_cursor_position()
+        TOX = calculate(cursor_x, x, SENSITIVITY)
+        TOY = calculate(cursor_y, y, SENSITIVITY)
+        cv.waitKey(1)
+        VM.move_mouse_relative(TOX, TOY)
+        try:
+            POSITIONS.pop(0)
+        except:
+            pass
+
+
+def display(event: Event):
+    while not event.is_set():
+        if FRAME is not None:
+            cv.arrowedLine(
+                FRAME,
+                ((FOV // 2), (FOV // 2)),
+                ((FOV // 2) + TOX, (FOV // 2) + TOY),
+                (0, 0, 0), 
+                5,
+                1,
+            )
+            for x, y in POSITIONS:
+                cv.rectangle(FRAME, (x, y), (x + 15, y + 15), (255, 0, 0), 3, 1)
+            cv.imshow("feed", FRAME)
+            cv.setWindowProperty("feed", cv.WND_PROP_FULLSCREEN, cv.WINDOW_NORMAL)
+            cv.setWindowProperty("feed", cv.WND_PROP_TOPMOST, 1)
+            cv.waitKey(1)
+
+
+def keyboard_event(event: kb.KeyboardEvent):
+    global stop_event
+    stop_event.set()
+
+
+if __name__ == "__main__":
+    stop_event = Event()
+    kb.hook_key("q", keyboard_event, suppress=True)
+    threads = [
+        Thread(target=screenshot, args=(stop_event,)),
+        Thread(target=detect, args=(stop_event,)),
+        Thread(target=move_aim, args=(stop_event,)),
+        Thread(target=display, args=(stop_event,)),
+    ]
+
+    for t in threads:
+        t.start()
+
+    stop_event.wait()
+    for t in threads:
+        t.join()
+    kb.unhook_all()
